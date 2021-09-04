0968ac50087b60a5d8b4fe7a094cc58218ea2274
Kaus <87383189+Kaus7@users.noreply.github.com>
2021-09-03 17:23:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.font.init()
 
 WIDTH, HEIGHT = 700,700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Shooter Tutorial")
+pygame.display.set_caption("Alien Shooting Game")
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
